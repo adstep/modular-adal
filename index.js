36a0764aaf92d7b6ea75d442d212d97137fa3965
jasteph <jasteph@uw.edu>
2017-09-19 23:55:40 -0700

    remove export

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 // Expose the AuthenticationContext import on the window object.
 // This makes it available for iframes calling window.parent.AuthenticationContext.
 window.AuthenticationContext = require('adal-angular/lib/adal.js');
-
-module.exports = window.AuthenticationContext;
